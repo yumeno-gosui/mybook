0579f6a5b75cbe45bbaf7f91ecb7b4f75411f513
yumeno-gosui <160216723+yumeno-gosui@users.noreply.github.com>
2026-04-06 19:10:51 +0900

    Update vivliostyle.config.js

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '日常が少しだけ異世界に染まる時間',
   author: '午睡キネマ <yumeno.gosui@gmail.com>',
   language: 'ja',
-  theme: 'css/style5.css',
+  theme: 'css/style_uno.css',
   entry: [
     "Uno.md",
   ],
